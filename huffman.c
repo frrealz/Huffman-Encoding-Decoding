27ad2185c975e7e09c96b294c0ee07708594dcd9
Samuel Wu <wusam6@gmail.com>
2017-06-04 22:05:44 -0700

    Update huffman.c

--- huffman.c
+++ huffman.c
@@ -14,16 +14,43 @@ treeNode *newNode(uint8_t s, bool l, uint64_t c)
 }
 
 // Dump a Huffman  tree  onto a file
-void  dumpTree(treeNode *t, int  file)
+void  dumpTree(treeNode *t, int file)
 {
-	//just to test
-	if(file)
-	{
-		
+	if (t && t->leaf)
+	{	
+		if(file == -1)		//writes to standard output
+		{
+			write(1, "L", 1);
+			if(write(1, &t->symbol, 8) != 8)
+			{
+				printf("error copying tree into file\n");
+				return;
+			}
+		}
+		else		//writes to file
+		{
+			write(file, "L", 1);
+			if(write(file, &t->symbol, 8) != 8)
+			{
+				printf("error copying tree into file\n");
+				return;
+			}
+		}
 	}
-	if(t)
+	else if (t)
 	{
-		
+		if(file == -1)		//writes to standard output
+		{
+			write(1, "I", 1);
+			dumpTree(t->left, file);
+			dumpTree(t->right, file);
+		}
+		else		//writes to file
+		{
+			write(file, "I", 1);
+			dumpTree(t->left, file);
+			dumpTree(t->right, file);
+		}
 	}
 	return;
 }
@@ -61,10 +88,11 @@ int32_t  stepTree(treeNode *root , treeNode  **t, uint32_t  code)
 }
 
 // Parse a Huffman  tree to  build  codes
-void  buildCode(treeNode *t, code s, code table[256])
+void  buildCode(treeNode *t, code s, code table[256], uint16_t *leafCount)
 {
 	if (t && t->leaf)
 	{
+		leafCount++;		//increments how many leaves
 		table[t->symbol] = s;
 		
 		//so you only get correct codes if you print in reverse
@@ -74,7 +102,7 @@ void  buildCode(treeNode *t, code s, code table[256])
 	{
 		printf("push 0\n");
 		pushCode(&s, 0);
-		buildCode(t->left, s, table);				//left side
+		buildCode(t->left, s, table, leafCount);				//left side
 		
 		
 		uint32_t pop = 2;
@@ -82,7 +110,7 @@ void  buildCode(treeNode *t, code s, code table[256])
 		popCode(&s, &pop);
 		printf("push 1\n");
 		pushCode(&s, 1);
-		buildCode(t->right, s, table);				//right side
+		buildCode(t->right, s, table, leafCount);				//right side
 	}
 	return;
 }
@@ -117,20 +145,20 @@ void printTree(treeNode *t, int depth)
 {
         if (t && t->leaf)
         {
-                if (isalnum(t->symbol))
-                {
-                        spaces(4 * depth); printf("%c (%lu)\n", t->symbol, t->count);
-                }
-                else
-                {
-                        spaces(4 * depth); printf("%X (%lu)\n", t->symbol, t->count);
-                }
+			if (isalnum(t->symbol))
+			{
+				spaces(4 * depth); printf("%c (%lu)\n", t->symbol, t->count);
+			}
+			else
+			{
+				spaces(4 * depth); printf("%X (%lu)\n", t->symbol, t->count);
+			}
         }
         else if (t)
         {
-                spaces(4 * depth); printf("$ (%lu)\n", t->count);
-                printTree(t->left, depth + 1);
-                printTree(t->right, depth + 1);
+			spaces(4 * depth); printf("$ (%lu)\n", t->count);
+			printTree(t->left, depth + 1);
+			printTree(t->right, depth + 1);
         }
         return;
 }
