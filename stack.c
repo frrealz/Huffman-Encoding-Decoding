731cf22a21585001c6d273f1800687c9803fd865
narking <narking@ucsc.edu>
2017-06-07 11:54:58 -0700

    Changed to a stack of pointers (tree node **)

--- stack.c
+++ stack.c
@@ -7,7 +7,7 @@ stack *newStack()
         stack *s = (stack *) calloc(MIN_STACK, sizeof(stack));
         s->size = MIN_STACK;
         s->top  = 0;
-        s->entries = (treeNode *) calloc(MIN_STACK, sizeof(treeNode));
+        s->entries = (treeNode **) calloc(MIN_STACK, sizeof(treeNode *));
         return s;
 }
 
@@ -18,12 +18,12 @@ treeNode *pop(stack *s)
         if (s->top > 0)
         {
             s->top -= 1;
-            return &s->entries[s->top];
+            return s->entries[s->top];
         }
         else
         {
             printf("Invalid in stack\n");
-            return &s->entries[s->top];
+            return s->entries[s->top];
         }
 }
 
@@ -32,9 +32,9 @@ void push(stack *s, treeNode *i)
         if (s->top == s->size)
         {
                 s->size *= 2;
-                s->entries = (treeNode *) realloc(s->entries, s->size * sizeof(treeNode));
+                s->entries = (treeNode **) realloc(s->entries, s->size * sizeof(treeNode *));
         }
-        s->entries[s->top] = *i;
+        s->entries[s->top] = i;
         s->top += 1;
         return;
 
