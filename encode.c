91a5968e03ed6b66664f803bb3bf472f532f417a
Samuel Wu <wusam6@gmail.com>
2017-06-07 19:03:54 -0700

    only memory leaks now

--- encode.c
+++ encode.c
@@ -20,6 +20,7 @@
 
 int main(int argc, char **argv)
 {
+	int verboseMode = 0;
 	uint32_t magicNumber = 0xDEADD00D;
 	
 	uint32_t histogram[256] = {0};		//histogram
@@ -31,6 +32,7 @@ int main(int argc, char **argv)
 	char *inputFile = NULL;
 	char *outputFile = NULL;
 	int c;
+
 	while ((c = getopt(argc, argv, "i:o:v")) != -1)
 	{
 		switch(c)
@@ -47,6 +49,7 @@ int main(int argc, char **argv)
 			}
 			case 'v':
 			{
+				verboseMode = 1;
 				break;
 			}
 			default:
@@ -95,7 +98,8 @@ int main(int argc, char **argv)
 	
 	//////////////////////////////////////////////////////////////////////////////////////////////////
 	//creating priority queue
-	queue *priorityQueue = newQueue(10000);
+	queue *priorityQueue = newQueue(300);
+	
 	
 	for(int i = 0; i < 256; i++)
 	{
@@ -107,24 +111,21 @@ int main(int argc, char **argv)
 			//printf("symbol: %x, count: %lu, leaf: %d\n", newTree->symbol, newTree->count, newTree->leaf);
 		}
 	}
-	printf("\n");
 	
 	
 	///////////////////////////////////////////////////////////////////////////////////////
 	//trying to make huffman tree
-	treeNode *root = newNode('$', 0, 0);
+	treeNode *root;
 	while(abs(priorityQueue->head - priorityQueue->tail) != 1)	//until only one item left in queue, which is root
 	{//joins two smallest items in queue
-		treeNode *leaf0 = malloc(sizeof(treeNode));
-		leaf0->left = malloc(sizeof(treeNode));
-		leaf0->right = malloc(sizeof(treeNode));
 		
+		treeNode *leaf0 = malloc(sizeof(treeNode));
 		treeNode *leaf1 = malloc(sizeof(treeNode));
-		leaf1->left = malloc(sizeof(treeNode));
-		leaf1->right = malloc(sizeof(treeNode));
 		
 		dequeue(priorityQueue, leaf0);
 		dequeue(priorityQueue, leaf1);
+
+
 		// printf("symbol: %x, count: %lu, leaf: %d\n", leaf0->symbol, leaf0->count, leaf0->leaf);
 		if(leaf0->symbol != 0x24)
 		{
@@ -135,26 +136,21 @@ int main(int argc, char **argv)
 			leaf1->leaf = 1;
 		}
 		
-		treeNode *internalNode = join(leaf0, leaf1);	//joins two leaf
+		root = join(leaf0, leaf1);	//joins two leaf
 		//debug
-		// printf("HEAD symbol: %c, count: %lu, leaf: %d\n", internalNode->symbol, internalNode->count, internalNode->leaf);
-		// printf("RIGHT symbol: %c, count: %lu, leaf: %d\n", internalNode->right->symbol, internalNode->right->count, internalNode->right->leaf);
-		// printf("LEFT symbol: %c, count: %lu, leaf: %d\n", internalNode->left->symbol, internalNode->left->count, internalNode->left->leaf);
-		enqueue(priorityQueue, *internalNode);
+		// printf("HEAD symbol: %c, count: %lu, leaf: %d\n", root->symbol, root->count, root->leaf);
+		// printf("RIGHT symbol: %c, count: %lu, leaf: %d\n", root->right->symbol, root->right->count, root->right->leaf);
+		// printf("LEFT symbol: %c, count: %lu, leaf: %d\n", root->left->symbol, root->left->count, root->left->leaf);
+		enqueue(priorityQueue, *root);
 		
 		//printf("symbol: %x, count: %lu, leaf: %d\n\n", priorityQueue->Q[3].symbol, priorityQueue->Q->count, priorityQueue->Q->leaf);
-		if(abs(priorityQueue->head - priorityQueue->tail) == 1)//set root when only one item left in queue
-		{
-			*root = *internalNode;
-		}
 	}
 		///////////////////////////////////////////////////////////////////////////////////////
 
-	
 
-	
-	
 	//delete priority Queue
+
+	delTree(priorityQueue->Q);
 	delQueue(priorityQueue);
 	///////////////////////////////////////////////////////////////////////////////////////
 	
@@ -191,12 +187,19 @@ int main(int argc, char **argv)
 	
 	uint16_t treeSize = 3*leafCount - 1;
 	//printf("leafCount: %u\n", leafCount);
-	int file1;
+	
+	
+	int file1 = open(outputFile, O_WRONLY);
+
+	if(file1 == -1)
+	{
+		perror(outputFile);
+		exit(errno);
+	}
+	
 	
 	if(outputFile)	//writes to outputFile
 	{
-		file1 = open(outputFile, O_WRONLY);
-		
 		write(file1, &magicNumber, 4);
 		write(file1, &inputFileLength, 8);
 		write(file1, &treeSize, 2);
@@ -215,6 +218,9 @@ int main(int argc, char **argv)
 		dumpTree(root, 1);
 	}
 	
+	delTree(root);
+	
+
 	
 	file = open(inputFile, O_RDONLY);
 	if(file == -1)
@@ -222,37 +228,27 @@ int main(int argc, char **argv)
 		perror(inputFile);
 		exit(errno);
 	}
-
+	
+	uint64_t bitsEncoded = 0;
 	uint8_t bitVector = 0;
 	int j = 0;
 
+	
+	
 	while( (size = read(file, buffer, 1)) > 0 )		//reads through file byte by byte
 	{
 
-		int index = (int) buffer[0];		//has value '0x0', '0x1', ... '0xFF'
+		uint8_t index = (int) buffer[0];		//has value '0x0', '0x1', ... '0xFF'
 		// printf("\ncode length: %u\n", codeTable[index].l);
 		//for(int i = (codeTable[index].l-1); i >= 0  ; i--)			//reverse order of bits
-		for(uint32_t i = 0; i < codeTable[index].l ; i++)
+		int max = codeTable[index].l;
+		for(int i = 0; i < max ; i++)
 		{
-			int bitVectorIndex = 0;
-			if(i < 8)				//chooses which array of bitvectors
-			{
-				bitVectorIndex = 0;
-			}
-			else if(8 <= i && i < 16)
-			{
-				bitVectorIndex = 1;
-			}
-			else if(16 <= i && i < 24)
-			{
-				bitVectorIndex = 2;
-			}
-			else if(24<= i && i < 32)
-			{
-				bitVectorIndex = 3;
-			}
-			
-			
+			int divisor = i;
+			int bitVectorIndex = 0;				//chooses which array of bitvectors
+			bitVectorIndex = divisor/32;				//works because takes floor of int division
+			//if(codeTable[index].l != 91755790 && codeTable[index].l != 8388991)
+			//printf("char: %x, bitVectorIndex: %d length %d, max: %u\n", buffer[0], bitVectorIndex, divisor, codeTable[index].l);
 			if( ((codeTable[index].bits[bitVectorIndex] & (1<<i))<<i) == 0)		//clear bit
 			{
 				//printf("clear %uth bit\n", j);
@@ -266,7 +262,7 @@ int main(int argc, char **argv)
 				//printf("\nbitvector is: %x\n", bitVector);
 			}
 			j++;
-			
+			bitsEncoded++;
 			if(j == 8)			//if bitVector is full
 			{
 				if(outputFile)
@@ -284,7 +280,9 @@ int main(int argc, char **argv)
 			}			
 		}
 	}
-	//printf("bitvector: x%x\n", bitVector);
+	
+	
+	
 	if(outputFile)			//prints out any remainder
 	{
 		write(file1, &bitVector, 1);
@@ -294,13 +292,6 @@ int main(int argc, char **argv)
 		write(1, &bitVector, 1);
 	}
 	
-
-	
-	if (size == -1)									//error checking for read()
-	{
-		perror(inputFile);
-		exit(errno);
-	}
 	
 	//closes file
 	size = close(file);
@@ -309,16 +300,19 @@ int main(int argc, char **argv)
 		perror(inputFile);
 		exit(errno);
 	}
-	
-	
-	printf("\n");
-	
+	size = close(file1);
+	if(size == -1)		
+	{
+		perror(inputFile);
+		exit(errno);
+	}
 		
-	
-	
-	
-	
-	
+
+		
+	if(verboseMode == 1)
+	{
+		printf("\nOriginal %lu bits: leaves %d (%u bytes) encoding %lu bits(%f%%)\n", inputFileLength*8, leafCount, treeSize, bitsEncoded, ((float)bitsEncoded*100/((float)inputFileLength*8)));
+	}
 	return 0;
 }
 
